199c40c9a4ab59fe051bc30841dcfb2a1947e37f
liugang416 <18317770662@163.com>
2020-08-10 02:45:06 +0800

    创建第一个pods

---
+++ LGNetWorking/Classes/LGNetworkManager.h
@@ -0,0 +1,17 @@
+//
+//  LGNetworkManager.h
+//  LGNetWorking_Example
+//
+//  Created by liugang on 2020/8/10.
+//  Copyright © 2020 liugang416. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface LGNetworkManager : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ LGNetWorking/Classes/LGNetworkManager.m
@@ -0,0 +1,13 @@
+//
+//  LGNetworkManager.m
+//  LGNetWorking_Example
+//
+//  Created by liugang on 2020/8/10.
+//  Copyright © 2020 liugang416. All rights reserved.
+//
+
+#import "LGNetworkManager.h"
+
+@implementation LGNetworkManager
+
+@end
